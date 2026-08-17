85992ac9922381db70d7558a4b8031a1cdbcb28c
Shravana Shegunashi <shravanashegunashi@gmail.com>
2026-08-17 23:12:09 +0530

    Update PrimeNumber.java

--- Basics/PrimeNumber.java
+++ Basics/PrimeNumber.java
@@ -1,1 +1,15 @@
+public class PrimeNumber {
+    public static void main(String[] args){
+        System.out.println(isPrime(12));
+    }
+    static boolean isPrime(int i) {
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
 
