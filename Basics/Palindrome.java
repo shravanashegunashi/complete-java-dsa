7411a6f21c5cd8a0b9fb0eb652a5933bc08e3289
Shravana Shegunashi <shravanashegunashi@gmail.com>
2026-08-18 12:17:53 +0530

    Update Palindrome.java

--- Basics/Palindrome.java
+++ Basics/Palindrome.java
@@ -1,1 +1,16 @@
+public class Palindrome {
+    public static void main() {
+        System.out.println(isPalindrome(1231));
+    }
+    static boolean isPalindrome(int n){
+        int original=n;
+        int ans=0;
+        while(n>0){
+            int rem=n%10;
+            ans=ans*10+rem;
+            n/=10;
+        }
+        return ans==original;
+    }
+}
 
