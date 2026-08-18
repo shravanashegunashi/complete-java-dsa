9b236f0c3798b2ac01fdcaa58e196e9e3487354d
Shravana Shegunashi <shravanashegunashi@gmail.com>
2026-08-18 12:14:56 +0530

    Update CountDigit.java

--- Basics/CountDigit.java
+++ Basics/CountDigit.java
@@ -1,1 +1,18 @@
+public class CountDigit {
+    public static void main() {
+        System.out.println(digit(1882838,8));
+    }
+    static int digit(int n,int target){
+        int c=0;
+        while (n>0){
+            int rem=n%10;
+            if(rem==target){
+                c++;
+            }
+            n/=10;
+        }
+        return c;
+    }
+}
+
 
