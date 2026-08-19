1588aaf199411b84edb081f991fd5c981dc70789
Shravana Shegunashi <shravanashegunashi@gmail.com>
2026-08-19 22:32:44 +0530

    Update Square.java

--- Patterns/Square.java
+++ Patterns/Square.java
@@ -1,1 +1,13 @@
-
+public class Square {
+    public static void main() {
+        square(4);
+    }
+    static void square(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
